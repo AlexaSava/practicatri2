b1a985b8250c7fa336c606de1574f89ef834daca
AlexaSava <alexandru.sava@tiernoparla.es>
2023-02-22 17:06:11 +0100

    se añade el precio de ventas y imprimir etiquetas

--- src/main/java/daw/practica/obra/Galeria.java
+++ src/main/java/daw/practica/obra/Galeria.java
@@ -2,9 +2,6 @@ package daw.practica.obra;
 
 import java.util.Scanner;
 
-import javax.sql.rowset.CachedRowSet;
-import javax.sql.rowset.serial.SerialArray;
-
 import daw.practica.AlmacenPorqueria.Fors;
 import daw.practica.AlmacenPorqueria.Todo;
 
@@ -14,51 +11,58 @@ public class Galeria {
      * @param args
      */
 
-    static final int VER_OBRAS = 1;
-    static final int DAR_ALTA_OBRA_NUEVA = 2;
-    static final int MODIFICAR_OBRA = 3;
-    static final int VISUALIZAR_OBRA_ARTE = 4;
-    static final int PRECIO_VENTAS = 5;
-    static final int NUMERO_ELEGIR_TOTAL = 7;
+    public static final int VER_OBRAS = 1;
+    public static final int DAR_ALTA_OBRA_NUEVA = 2;
+    public static final int MODIFICAR_OBRA = 3;
+    public static final int VISUALIZAR_OBRA_ARTE = 4;
+    public static final int PRECIO_VENTAS = 5;
+    public static final int IMPRIMIR_ETIQUETA = 6;
+    public static final int NUMERO_ELEGIR_TOTAL = 7;
+    public static final int PORCENTAJE = 25;
+    public static final int PORCENTAJE_PESO_MAX = 100;
+    public static final int PORCENTAJE_PESO_MIN = 20;
+    public static final int PORCENTAJE_ALTURA_MAX = 100;
+    public static final int PORCENTAJE_ALTURA_MIN = 20;
+    public static final int ADICIONALES = 10;
 
-    public static void main(String[] args){
-    
-    
+    public static void main(String[] args) {
+
         Scanner pepe = new Scanner(System.in);
         System.out.println("Eliga una opción");
 
         Todo.Cosas();
         int elegir = 0;
         while (elegir != NUMERO_ELEGIR_TOTAL) {
-            elegir = pepe.nextInt();
             
-        
-                switch (elegir){
-                    case VER_OBRAS:
+            elegir = pepe.nextInt();
+
+            switch (elegir) {
+                case VER_OBRAS:
                     Fors.LeerPinturas();
                     Fors.LeerEsculturas();
                     continue;
 
-                    case DAR_ALTA_OBRA_NUEVA:
+                case DAR_ALTA_OBRA_NUEVA:
                     Todo.AltaNuevaObra();
                     continue;
 
-                    case MODIFICAR_OBRA:
+                case MODIFICAR_OBRA:
                     Todo.ModificarObra();
                     continue;
-                    
-                    case VISUALIZAR_OBRA_ARTE:
+
+                case VISUALIZAR_OBRA_ARTE:
                     Todo.VisualizarObras();
                     continue;
-                }
 
-                
+                case PRECIO_VENTAS:
+                Todo.VentasPrecio();
 
-                
+                case IMPRIMIR_ETIQUETA:
+                Todo.ImprimirEtiqueta();
             }
-            
-           
 
         }
 
-        }
+    }
+
+}
